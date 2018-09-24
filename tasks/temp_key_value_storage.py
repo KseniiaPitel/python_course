f4762c86339bbc9a81562ce5ee688d7a26151df0
kspi <ksen!a.pitel@gmail.com>
2018-09-24 16:51:36 +0300

    key-values storage without argparse module

--- tasks/temp_key_value_storage.py
+++ tasks/temp_key_value_storage.py
@@ -2,17 +2,38 @@ import json
 import tempfile
 import os
 
-key = int(input("Key: "))
-val = input("Val: ")
-frame = {key: val}
-
-
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-with open(storage_path, 'a+b') as f:
-    f.write(bytes(json.dumps(frame, allow_nan=True, indent=None), 'UTF-8'))
-    f.seek(0)
-    print(f.read().decode())
 
-    print(storage_path)
+def is_non_zero_file(fpath):
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
+
+
+if is_non_zero_file(storage_path) == False:
+    frame = {}
+
+else:
+    with open(storage_path, 'r') as f:
+        frame = json.load(f)
+
+key = input("Key: ")
+val = input("Val: ")
+
+#frame[key] = val
+
+if key in frame:
+    temp_val = frame.get(key)
+    frame.update({key: temp_val + ", " + val})
+    with open(storage_path, 'w') as f:
+        (f.write(json.dumps(frame, indent=1)))
+else:
+    frame[key] = val
+    with open(storage_path, 'w') as f:
+        (f.write(json.dumps(frame, indent=1)))
+
+print(frame)
+
+search_key=input("Search key: ")
+search_result = frame.get(search_key)
+print("Searched values: ", search_result)
 
 

a1fd711e3d989b37b3c2e9ea53d666eccce18b75
kspi <ksen!a.pitel@gmail.com>
2018-09-25 13:37:00 +0300

    final commit of key-values storage task

--- tasks/temp_key_value_storage.py
+++ tasks/temp_key_value_storage.py
@@ -1,12 +1,9 @@
 import json
 import tempfile
 import os
-import sys
 import argparse
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-print(storage_path)
-
 
 def is_non_zero_file(fpath):
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
@@ -19,41 +16,33 @@ else:
     with open(storage_path, 'r') as f:
         frame = json.load(f)
 
+#проверка что был передан только Key
 
-def createParser():
+def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', '-k', '--key', required=True)
-    parser.add_argument('--val')
+    parser.add_argument('--key', '-key', action='store')
+    parser.add_argument('--val', '-val', action='store')
+    args = parser.parse_args()
+    return args.key, args.val
 
-    return parser
 
+val_check = create_parser()
 
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
-    key=str(namespace.name)
-    val=str(namespace.val)
+if val_check[1] is None:
+    search_result = frame.get(val_check[0])
+    print(search_result)
 
-# key = input("Key: ")
-# val = input("Val: ")
-# frame[key] = val
-
-if key in frame:
-    temp_val = frame.get(key)
-    frame.update({key: temp_val + ", " + val})
-    with open(storage_path, 'w') as f:
-        (f.write(json.dumps(frame, indent=1)))
+#запись в файл если было передано два значения key val
 else:
-    frame[key] = val
-    with open(storage_path, 'w') as f:
-        (f.write(json.dumps(frame, indent=1)))
+    key = val_check[0]
+    val = val_check[1]
 
-#print(frame)
-
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
-
-search_key = str(namespace.name)
-search_result = frame.get(search_key)
-print(search_result)
+    if key in frame:
+        temp_val = frame.get(key)
+        frame.update({key: temp_val + ", " + val})
+        with open(storage_path, 'w') as f:
+            (f.write(json.dumps(frame, indent=1)))
+    else:
+        frame[key] = val
+        with open(storage_path, 'w') as f:
+            (f.write(json.dumps(frame, indent=1)))

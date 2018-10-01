5598513c6dd8df65f601da1368efff7344a2a0ae
kspi <ksen!a.pitel@gmail.com>
2018-10-01 10:46:06 +0300

    decorator to json solution with functools.wraps

--- functional programming tasks/decorators_to_json.py
+++ functional programming tasks/decorators_to_json.py
@@ -3,23 +3,23 @@ from functools import wraps
 
 def to_json(func_to_decorate):
     @wraps(func_to_decorate)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         import json
-        updating = func_to_decorate()
+        updating = func_to_decorate(*args, **kwargs)
         updating = json.dumps(updating)
         return updating
 
     return wrapper
-
 
 """
 @to_json
 def get_data():
     return {
         'data111': 4444
-   }
+    }
 
-print("Decorated func ", get_data())
-print (get_data.__name__)
-print(get_data.__doc__) 
+
+print(get_data())
+print(get_data.__name__)
+print(get_data.__doc__)
 """

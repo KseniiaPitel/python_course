2fe848a6405fddec60b550ab59dded00376668e8
kspi <ksen!a.pitel@gmail.com>
2018-10-02 12:54:57 +0300

    implemented classes with printing obj as python strings

--- OOP tasks/cars.py
+++ OOP tasks/cars.py
@@ -7,26 +7,18 @@ class CarBase:
     def __str__(self):
         return self.brand
 
-
     def get_photo_file_ext(self):
         import os
         dirname, extname = os.path.splitext(self.photo_file_name)
         return extname
 
 
-#basecar = CarBase("Nisan", "C:\python project\python_course\OOP tasks\draft.py", 2.5)
-#print(basecar.get_photo_file_ext())
-#print(basecar.carrying)
-
-
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(brand, photo_file_name, carrying, passenger_seats_count)
+        super().__init__(brand, photo_file_name, carrying)
+        self.passenger_seats_count = passenger_seats_count
         self.car_type = "car"
 
-
-#car = Car("C:\python project\sample.txt")
-#print(Car.__dict__)
 
 
 class Truck(CarBase):
@@ -52,8 +44,6 @@ class Truck(CarBase):
             body_volume = body_width * body_height * body_length
             return body_volume
 
-#mytruck = Truck("10x15x11.2")
-#print(mytruck.get_body_volume())
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
@@ -64,20 +54,25 @@ class SpecMachine(CarBase):
 
 def get_car_list(csv_filename):
     import csv
-    f = open(csv_filename, 'r')
-
-    car_list = []
-    with f:
-        reader = csv.reader(f, delimiter=';')
-
+    with open(csv_filename, encoding="utf8") as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        raw_car_list = []
         for row in reader:
-            next(reader)
-            car_list = row
-            if car_list[0] == "car":
-                new_car = Car(car_list[2], car_list[3], car_list[5])
-                print(new_car)
-            print(car_list[0])
+            if len(row) == 0:
+                continue
+            if row[0] == "car":
+                new_car = Car(brand=row[1], photo_file_name=row[3], carrying=row[5], passenger_seats_count=row[2])
+                raw_car_list.append(new_car)
+            elif row[0] == "truck":
+                new_truck = Truck(brand=row[1], photo_file_name=row[3], carrying=row[5], body_whl=row[4])
+                raw_car_list.append(new_truck)
+            elif row[0] == "spec_machine":
+                new_machine = SpecMachine(brand=row[1], photo_file_name=row[3], carrying=row[5], extra=row[6])
+                raw_car_list.append(new_machine)
 
-    return car_list
 
-print(get_car_list("C:\python project\coursera_week3_cars(1).csv"))
+    return raw_car_list
+
+
+print(get_car_list("C:\python project\origin-file.csv"))
--- OOP tasks/load_from_csv.py
+++ OOP tasks/load_from_csv.py
@@ -1,23 +1,32 @@
+class CarBase:
+    def __init__(self, brand, photo_file_name, carrying):  # brand, carrying
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+        self.car_type = "base"
+
+    def __str__(self):
+        return self.brand, self.carrying, self.photo_file_name
+
 import csv
 
-
-f = open("C:\python project\coursera_week3_cars(1).csv", 'r')
-
-my_list = []
-with f:
-    reader = csv.reader(f, delimiter=';')
-
+with open("C:\python project\origin-file.csv", encoding="utf8") as csv_fd:
+    reader = csv.reader(csv_fd, delimiter=';')
+    next(reader)  # пропускаем заголовок
+    raw_car_list = []
     for row in reader:
-        next(reader)
-        if 'truck' in row:
-            my_list = row
-            print(my_list)
+        if len(row) == 0:
+            continue
+        if row[0] == "car":
+            new_car = CarBase(brand=row[1], photo_file_name=row[3], carrying=row[5])
+            raw_car_list.append(new_car)
+        elif row[0] == "truck":
+            new_truck = CarBase (brand=row[1], photo_file_name=row[3], carrying=row[5])
+            raw_car_list.append(new_truck)
+        elif row[0] == "spec_machine":
+            new_machine = CarBase (brand=row[1], photo_file_name=row[3], carrying=row[5])
+            raw_car_list.append(new_machine)
 
 
-print(type(my_list))
 
-
-
-
-
-
+print(raw_car_list)
